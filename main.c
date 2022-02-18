851219cdb2ee0c669803783cfa8254b76a3aa0c8
DennisOtuomaZetech <97729507+DennisOtuomaZetech@users.noreply.github.com>
2022-02-18 19:58:15 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+     Project: County Library Management System.
+     Author: Dennis Jesse Otuoma
+     Date: Feb,2022
+     Compiler: GNU GCC
+     Language: C99
+     License: MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("Welcome Dennis Jesse Otuoma\n");
+    printf("What would you like to do?\n");
+    printf("1. Add New User\n");
+    printf("2. Add Book.\n");
+    printf("Selected Action: ");
+    return 0;
+}
